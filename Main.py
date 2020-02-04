d73e57b45204f443046ded0b07cb411a5ebd1ae0
Harry G. Dulaney  IV <20808723+HarryDulaney@users.noreply.github.com>
2020-02-04 01:18:03 -0500

    init

--- Main.py
+++ Main.py
@@ -4,15 +4,11 @@ import urllib.request
 import PySimpleGUI as sg
 
 
-def handleScrape(url):
-        window['output'].update()
-        targetUrl = urllib.request.urlopen(url, [None])
-        soup = BeautifulSoup(targetUrl, 'html.parser')
-        window['output'].update(soup.prettify())
-        layout.append(sg.Button('Submit Filter'))
-
-
-
+def handleScrape(url, e1, e2):
+    targetUrl = urllib.request.urlopen(url)
+    soup = BeautifulSoup(targetUrl, 'html.parser')
+    e1.update('Pull Data From Selected Tags')
+    e2.update(soup.prettify())
 
 
 sg.theme('Material2')
@@ -20,21 +16,20 @@ layout = [
     [sg.Text('Input the URL that you want to scrape')],
     [sg.InputText(key='input')],
     [sg.Button('Scrape'), sg.Exit('Exit')],
-    [sg.Output(size =(80,20), key='output')]
+    [sg.Output(size=(80, 20), key='output')]
 ]
 
 window = sg.Window('Web-Scraper-Python', layout)
 
 
 while True:  # Event Loops
-    event, values = window.read(timeout=10)
-    print(event, values)
-    if event in (None, 'Exit'):
+    event, values = window.read()
+    if event == 'Exit':
         break
     if event == 'Scrape':
         url = values['input']
-        handleScrape(url)
+        e1 = window['Scrape']
+        e2 = window['output']
+        handleScrape(url, e1, e2)
 
-
-
-
+window.close()
