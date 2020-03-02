0253d24beaa437d326819f87241e1b9d258a4d70
HarryDulaney <harrydulaney@gmail.com>
2020-03-01 21:40:11 -0500

    datafetch working with json normalize function

--- CmdLineUI/data-fetch.py
+++ CmdLineUI/data-fetch.py
@@ -1,11 +1,11 @@
 import requests
 import pandas as pd
-from pandas.io.json import json_normalize
 
 
 print()
 print()
-# Test api connection and print affirmation
+
+# Begin test the api connection and print affirmation
 headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Token 39ac3ca9121471163ff7fe9bb6705e87a72b6352'
@@ -15,11 +15,11 @@ requestResponse = requests.get("https://api.tiingo.com/api/test/",
 responseDict = requestResponse.json()
 if responseDict.get('message') == 'You successfully sent a request':
     print('***Connection established, ready to get stock data!****')
+# End test
 
 print()
-
 print("Welcome to Stock Info Finder!")
-print("-------------------------------------------------------------------------a--")
+print("-----------------------------------------------------------------------------")
 print("Enter the Ticker Symbol for the stock you would like to fetch: ")
 ticker = str(input())
 
@@ -34,6 +34,9 @@ resturl = "https://api.tiingo.com/tiingo/daily/{}/prices?startDate={}&endDate={}
 requestResponse = requests.get(resturl.format(
     ticker, start_date, end_date), headers=headers)
 
-ready_data = requestResponse.__reduce__()
+ready_data = requestResponse.json()
 
-json_normalize(ready_data)
+normalized = pd.json_normalize(ready_data)
+
+print(normalized)
+
